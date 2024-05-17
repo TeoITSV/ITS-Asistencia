a631af4eb027dc623a1050eaf55c11bc7bd2cc1c
TeoITSV <t.reyna@itsv.edu.ar>
2024-05-17 10:11:23 -0300

    😀Profile pic generator

--- ITS-Asistencias/asistencias/models.py
+++ ITS-Asistencias/asistencias/models.py
@@ -1,5 +1,11 @@
 from django.db import models
-
+from django.core.files.base import ContentFile
+import string
+import random
+from PIL import Image, ImageDraw, ImageFont
+import io
+import os
+from django.conf import settings
 # Create your models here.
 class Base(models.Model):
     fechaCreacion = models.DateTimeField(auto_now_add=True);
@@ -9,12 +15,57 @@ class Base(models.Model):
     def __str__(self):
         return str(self.id);
 class Empleado(Base):
-    idEmpleado = models.IntegerField(primary_key=True);
-    nombreCompleto = models.CharField(max_length=100);
-    estaActivo = models.BooleanField(default=True);
-    
+    idEmpleado = models.IntegerField(primary_key=True)
+    nombreCompleto = models.CharField(max_length=100)
+    estaActivo = models.BooleanField(default=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+
     def __str__(self):
-        return self.nombreCompleto;
+        return self.nombreCompleto
+
+    def save(self, *args, **kwargs):
+        if not self.profile_picture:
+            self.profile_picture = self.generate_profile_picture()
+        super().save(*args, **kwargs)
+
+    def generate_profile_picture(self):
+        # Generar un color de fondo aleatorio
+        def random_color():
+            return "#{:06x}".format(random.randint(0, 0xFFFFFF))
+
+        # Obtener iniciales del nombre
+        def get_initials(name):
+            return ''.join([word[0].upper() for word in name.split()][:2])
+
+        initials = get_initials(self.nombreCompleto)
+        bg_color = random_color()
+        font_size = 80
+        img_width = 200
+        img_height = 200
+        image_size = (img_width, img_height)
+
+        # Crear imagen
+        image = Image.new('RGB', image_size, bg_color)
+        draw = ImageDraw.Draw(image)
+
+        # Ruta a la fuente
+        font_path = os.path.join(settings.BASE_DIR, 'fonts', 'PoetsenOne-Regular.ttf')
+
+        # Cargar fuente
+        try:
+            font = ImageFont.truetype(font_path, font_size)
+        except OSError:
+            raise OSError(f"Cannot open font resource at {font_path}")
+
+        # Dibujar texto
+        draw.text((img_width/2,img_height/2), initials, font=font, fill=(255, 255, 255),anchor="mm",align="center")
+
+        # Guardar imagen en un objeto BytesIO
+        image_io = io.BytesIO()
+        image.save(image_io, format='PNG')
+        image_content = ContentFile(image_io.getvalue(), f"{self.idEmpleado}_{self.nombreCompleto}_profile_picture.png")
+
+        return image_content
 class Horario(Base):
     esActual = models.BooleanField(default=True);
     empleado = models.ForeignKey('Empleado', on_delete=models.CASCADE);
